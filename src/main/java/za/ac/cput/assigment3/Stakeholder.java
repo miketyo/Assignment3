d229410db64b1e0fcdbc0334c7f908439ee01a8b
Mike <mikesomelezotyolani41@gmail.com>
2021-06-09 19:25:06 +0200

    Stakeholder class created

---
+++ src/main/java/za/ac/cput/assigment3/Stakeholder.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assigment3;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Mike Somelezo Tyolani (220187568)
+ */
+public class Stakeholder implements Serializable {
+     private String stHolderId;
+
+    public Stakeholder() {
+    }
+    
+    public Stakeholder(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+    
+    public String getStHolderId() {
+        return stHolderId;
+    }
+
+    public void setStHolderId(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+
+    @Override
+    public String toString() {
+       return stHolderId;
+    }
+
+
+    
+}
